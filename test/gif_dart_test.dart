ddbdbc7ef0d95c41cb02987e4a9c37145ffe26db
dennougorilla <dennougorilla@gmail.com>
2019-11-22 17:39:43 +0900

    add base

--- test/gif_dart_test.dart
+++ test/gif_dart_test.dart
@@ -1,16 +1,18 @@
+import 'dart:html';
+
 import 'package:gif_dart/gif_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    Awesome awesome;
+    GIF gif;
 
     setUp(() {
-      awesome = Awesome();
+      gif = GIF(Options());
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(gif.addFrame(CanvasElement(),AddFrameOptions()), isEmpty);
     });
   });
 }
